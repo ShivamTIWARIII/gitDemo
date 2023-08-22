7ea25260f91267dcbfee52d145ab3b4f73a4eca7
ShivamTIWARIII <shivamtiwariii1212@gmail.com>
2023-08-22 11:12:00 +0530

    second file

---
+++ Sharpner projects/DM.js
@@ -0,0 +1,6 @@
+var title=document.getElementById('header-title')
+title.style.borderBottom="3px solid black"
+
+var items=document.getElementsByClassName('title')
+items.style.color='green'
+items.style.fontWeight='bold'
